22f038833741c43b0358e3032f75eb56fb989f15
Jhen Nunez <jxelektro@gmail.com>
2022-08-17 23:54:24 -0400

    css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-/*if (navigator.geolocation) {
+if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function (position) {
     console.log(position);
     
@@ -10,12 +10,12 @@
   //Caso NO soporta geolocalización
   alert('Navegador NO soporta geolocalización');
 }
-*/
+
 
 let lat = -33.45;
 let lon = -70.64;
-let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=985e85838a70919538d2f1a260350ffd`
-
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=sp&appid=985e85838a70919538d2f1a260350ffd`
+let icon = "10d" 
 console.log(url)
 
 axios.get(url)
@@ -23,10 +23,11 @@ axios.get(url)
   .then(response => {
     this.users = response.data;
     let clima = response.data;
-    let estado = ""
-    let result = `<li>Ciudad:${response.data.name}</li><li>Temperatura:${Math.floor((response.data.main.temp) / 33)}°</li><li>Min: ${Math.floor((response.data.main.temp_min) / 33)}°</li><li>Max: ${Math.floor((response.data.main.temp_min) / 33)}°</li>`
+    
+    let result = `<h1>${response.data.name}</h1><li>${response.data.weather[0].description}</li><li>Temperatura:${Math.floor((response.data.main.temp) / 33)}°</li><li>Min: ${Math.floor((response.data.main.temp_min) / 33)}°</li><li>Max: ${Math.floor((response.data.main.temp_min) / 33)}°</li>`
+    let img = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
     document.getElementById("container").innerHTML = result
-console.log(clima)
+    document.getElementById("inner-skew").innerHTML = img
   })
   .catch(e => {
     // Podemos mostrar los errores en la consola
