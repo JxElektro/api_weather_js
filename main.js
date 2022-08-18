76a962098baaf0663dc6136266297229f1011c50
Jhen Nunez <jxelektro@gmail.com>
2022-08-18 03:26:59 -0400

    Se convirtio a celsios

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ console.log(search)
 
 let city = "japon"
 let key = "ef84c9f176e654737a9cb10d2927de6c"
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
 
 
 
@@ -19,7 +19,7 @@ axios.get(url)
     let clima = response.data;
     let icon = response.data.weather[0].icon 
     let dato = response.data.weather[0].description
-    let result = `<h1>${response.data.name}</h1><li>${response.data.weather[0].description}</li><li>Temperatura:${Math.floor((response.data.main.temp))}°</li><li>Min: ${Math.floor(response.data.main.temp_min)}°</li><li>Max: ${Math.floor(((response.data.main.temp_max))/33) }°</li>`
+    let result = `<h1>${response.data.name}</h1><li>${response.data.weather[0].description}</li><br><li>Temperatura:<h2>${Math.floor((response.data.main.temp))}°<h2></li><li>Min: ${Math.floor(response.data.main.temp_min)}°</li><li>Max: ${Math.floor(response.data.main.temp_max) }°</li>`
     let img = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
     document.getElementById("container").innerHTML = result
     document.getElementById("inner-skew").innerHTML = img
