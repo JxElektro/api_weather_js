00cac86877eff8323492f9e994276e7d2558a368
Jhen Nunez <jxelektro@gmail.com>
2022-08-18 01:31:48 -0400

    css listo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-if (navigator.geolocation) {
+/*if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function (position) {
     console.log(position);
 
@@ -9,7 +9,7 @@ if (navigator.geolocation) {
 } else {
   //Caso NO soporta geolocalización
   alert('Navegador NO soporta geolocalización');
-}
+}*/
 
 // actualizar lat y long
 let lat = -33.45;
@@ -17,25 +17,24 @@ let lon = -70.64;
 let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=en&appid=985e85838a70919538d2f1a260350ffd`
 
 let data
+
 // que eliga entre iconos
 let icon ="01d"
-if (data === "clear sky") {
-  icon = "01d"
-} else if (data === "few clouds") {
+if (data === "few clouds") {
   icon = "02d"
-} else if (data === "scattered clouds") {
+} else if (data == "scattered clouds") {
   icon = "03d"
-} else if (data === "broken clouds") {
+} else if (data == "broken clouds") {
   icon = "09d"
-} else if (data === "shower rain") {
+} else if (data == "shower rain") {
   icon = "10d"
-} else if (data === "rain") {
+} else if (data == "rain") {
   icon = "11d"
-} else if (data === "thunderstorm") {
+} else if (data == "thunderstorm") {
   icon = "08d"
-} else if (data === "snow") {
+} else if (data == "snow") {
   icon = "13d"
-} else if (data === "mist") {
+} else if (data == "mist") {
   icon = "50d"
 }
 
@@ -49,14 +48,14 @@ axios.get(url)
   .then(response => {
     this.users = response.data;
     let clima = response.data;
-    const data = response.data.weather[0].description
+    data = response.data.weather[0].description
     let result = `<h1>${response.data.name}</h1><li>${response.data.weather[0].description}</li><li>Temperatura:${Math.floor((response.data.main.temp) / 33)}°</li><li>Min: ${Math.floor((response.data.main.temp_min) / 33)}°</li><li>Max: ${Math.floor((response.data.main.temp_min) / 33)}°</li>`
     let img = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
     document.getElementById("container").innerHTML = result
     document.getElementById("inner-skew").innerHTML = img
 
+    console.log(data)
 
-console.log(data)
 
   })
   .catch(e => {
